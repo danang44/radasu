2901d3a80054276cd56ea1cbc014b5129bb43ead
danang44 <51013416+danang44@users.noreply.github.com>
2022-08-24 21:39:00 +0700

    framework
    template

--- resources/views/Admin/home.blade.php
+++ resources/views/Admin/home.blade.php
@@ -1,11 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-<body>
-    <button class="btn btn-info">Tes Button</button>
-</body>
-</html>
+
+ @extends('master.admin_layout')
+
+@section('content')
+
+
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card text-center">
+                <div class="card-header">Dashboard</div>
+                    
+                 
+
+                <!-- <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif -->
+
+                    HALLO ADMIN SEWAIN ID SELAMAT DATANG
+                    <BR></BR>
+                    HAVE A NICE DAY 
+                </div>
+            </div>
+        </div>
+        <img src="{{ asset('../images/vyruss.png') }}" width = 500px;>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts_admin/sidebar.blade.php
@@ -0,0 +1,96 @@
+<div class="sidebar sidebar-light sidebar-main sidebar-expand-md">
+    
+
+<!-- Sidebar mobile toggler -->
+<div class="sidebar-mobile-toggler text-center">
+    <a href="#" class="sidebar-mobile-main-toggle">
+        <i class="icon-arrow-left8"></i>
+    </a>
+    Navigation
+    <a href="#" class="sidebar-mobile-expand">
+        <i class="icon-screen-full"></i>
+        <i class="icon-screen-normal"></i>
+    </a>
+</div>
+<!-- /sidebar mobile toggler -->
+
+
+<!-- Sidebar content -->
+<div class="sidebar-content">
+    
+    <!-- User menu -->
+    <div class="sidebar-user">
+        <div class="card-body">
+            <div class="media">
+                <div class="mr-3">
+                    <a href="#"> <img src="{{ asset('../images/tiber.png') }}" width="50" height="50" class="rounded-circle" alt=""></a>
+                </div>
+
+                <div class="media-body">
+                    <div class="media-title font-weight-semibold">TIBER PANGESTU</div>
+                    <div class="font-size-xs opacity-50">
+                        <i class="icon-pin font-size-sm"></i> &nbsp;Semarang, IDN
+                    </div>
+                </div>
+
+               
+            </div>
+        </div>
+    </div>
+    <!-- /user menu -->
+
+    
+    <!-- Main navigation -->
+    <div class="card card-sidebar-mobile">
+        <ul class="nav nav-sidebar" data-nav-type="accordion">
+
+            <!-- Main -->
+            <li class="nav-item-header"><div class="text-uppercase font-size-xs line-height-xs">Main</div> <i class="icon-menu" title="Main"></i></li>
+            <li class="nav-item">
+                <a href="/home" class="nav-link active">
+                    <i class="icon-home4"></i>
+                    <span>
+                        Dashboard
+                        <span class="d-block font-weight-normal opacity-50">TIBER PANGESTU</span>
+                    </span>
+                </a>
+            </li>
+            <li class="nav-item nav-item-submenu nav-item-open">
+							<a href="#" class="nav-link"><i class="icon-copy"></i> <span>E-Comerce</span></a>
+
+							<ul class="nav nav-group-sub" data-submenu-title="Layouts" style="display: block;">
+                                <li class="nav-item"><a href="/kelolaberita" class="nav-link">BERITA</a></li> 
+                                <!-- <li class="nav-item"><a href="/kelolamateri" class="nav-link">MATERI</a></li> 
+                                <li class="nav-item"><a href="/kelolamitra" class="nav-link">MITRA</a></li> 
+                                <li class="nav-item"><a href="/kelolaproyek" class="nav-link">PROYEK DISHUB</a></li> 
+                                <li class="nav-item"><a href="/kelolapendidikan" class="nav-link">PROYEK PENDIDIKAN</a></li> 
+                                <li class="nav-item"><a href="/kelolaswasta" class="nav-link">PROYEK SWASTA</a></li> 
+                                <li class="nav-item"><a href="/kelolapemerintah" class="nav-link">PROYEK PEMERINTAH</a></li> 
+                                <li class="nav-item"><a href="/kelolaproduk" class="nav-link">PRODUK</a></li> 
+                                <li class="nav-item"><a href="/kelolaslider" class="nav-link">SLIDER</a></li> 
+                                <!-- <li class="nav-item"><a href="/aboutcontent" class="nav-link">About Content</a></li> -->
+								<!-- <li class="nav-item"><a href="/produk" class="nav-link">Produk</a></li> -->
+								<!-- <li class="nav-item"><a href="/kategori" class="nav-link">Kategori</a></li> -->
+								<!-- <li class="nav-item"><a href="/order" class="nav-link">Order</a></li> -->
+								<li class="nav-item"><a href="/pesanan" class="nav-link">Pesanan</a></li>
+								<!-- <li class="nav-item"><a href="/kirim" class="nav-link">Pengiriman</a></li> -->
+						
+                                                   
+                                       
+                                       
+								<!-- <li class="nav-item"><a href="../../../../layout_6/LTR/default/full/index.html" class="nav-link disabled">Layout 6 <span class="badge bg-transparent align-self-center ml-auto">Coming soon</span></a></li> -->
+                                <!-- <li class="nav-item"><a href="/tes" class="nav-link">TES</a></li> -->
+							</ul>
+						</li>
+
+                        
+         
+                
+        </ul>
+    </div>
+    <!-- /main navigation -->
+
+</div>
+<!-- /sidebar content -->
+
+</div>
